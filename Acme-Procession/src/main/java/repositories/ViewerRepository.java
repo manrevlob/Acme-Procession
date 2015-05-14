525892bb4cac38f86f4bc5423b1728ef41f30658
Manuel Revilla <manrevlob@users.noreply.github.com>
2015-05-14 14:00:14 +0000

    Modificado servicio y repositorio de viewer por conflictos.

--- Acme-Procession/src/main/java/repositories/ViewerRepository.java
+++ Acme-Procession/src/main/java/repositories/ViewerRepository.java
@@ -1,11 +1,15 @@
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Viewer;
 
 @Repository
 public interface ViewerRepository extends JpaRepository<Viewer, Integer> {
+
+	@Query("select v from Viewer v where v.userAccount.id = ?1")
+	Viewer findByPrincipal(int userAccountId);
 
 }
