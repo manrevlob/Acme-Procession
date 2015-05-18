0bee84ea6f202e6aa1a2dd41d1cea47adcd44786
Manuel Revilla <manrevlob@users.noreply.github.com>
2015-05-18 14:24:18 +0000

    Añadido caso de uso de registrar un hermano a un tramo disponible

--- Acme-Procession/src/main/java/controllers/brother/StretchOrderBrotherController.java
+++ Acme-Procession/src/main/java/controllers/brother/StretchOrderBrotherController.java
@@ -10,9 +10,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import services.ProcessionService;
+import services.RegistrationService;
 import services.StretchOrderService;
 import controllers.AbstractController;
 import domain.Procession;
+import domain.Registration;
 import domain.StretchOrder;
 
 @Controller
@@ -20,74 +22,111 @@ import domain.StretchOrder;
 public class StretchOrderBrotherController extends AbstractController {
 
 	// Services ---------------------------------------------------------------
-	
+
 	@Autowired
 	private StretchOrderService stretchOrderService;
-	
+	@Autowired
+	private RegistrationService registrationService;
 	@Autowired
 	private ProcessionService processionService;
-	
+
 	// Constructors -----------------------------------------------------------
-	
+
 	public StretchOrderBrotherController() {
 		super();
 	}
-	
+
 	// Listing ----------------------------------------------------------------
-	
+
+	// Register ---------------------------------------------------------------
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public ModelAndView list(@RequestParam int stretchOrderId) {
+		ModelAndView result;
+		StretchOrder stretchOrder;
+		Registration registration;
+		String error;
+
+		stretchOrder = stretchOrderService.findOne(stretchOrderId);
+		registration = registrationService.create(stretchOrder);
+		error = null;
+
+		try{
+			registrationService.save(registration);
+		}catch(Throwable oops){
+			if (oops.getMessage().equals("registration.otherRegistrastionCreated.error")) {
+				error = "registration.otherRegistrastionCreated.error";
+			}
+		}
+		
+		result = new ModelAndView(
+				"redirect:/stretchOrder/list.do?processionId="
+						+ stretchOrder.getProcession().getId());
+		result.addObject("error", error);
+
+		return result;
+	}
+
 	// Creation ---------------------------------------------------------------
 
 	// Edition ----------------------------------------------------------------
 
 	@RequestMapping(value = "/moveToUp", method = RequestMethod.GET)
-	public ModelAndView moveToUp(@RequestParam int stretchOrderId, @RequestParam int processionId) {
+	public ModelAndView moveToUp(@RequestParam int stretchOrderId,
+			@RequestParam int processionId) {
 		ModelAndView result;
 		Procession procession;
 		StretchOrder stretchOrder;
 		Collection<StretchOrder> stretchOrders;
-		
+
 		procession = processionService.findOne(processionId);
 		stretchOrder = stretchOrderService.findOne(stretchOrderId);
 		stretchOrders = stretchOrderService.findByProcession(procession);
 		stretchOrderService.moveToUp(stretchOrder, stretchOrders);
-		
-		result = new ModelAndView("redirect:/stretchOrder/list.do?processionId=" + processionId);
-		
+
+		result = new ModelAndView(
+				"redirect:/stretchOrder/list.do?processionId=" + processionId);
+
 		return result;
 	}
-	
+
 	@RequestMapping(value = "/moveToDown", method = RequestMethod.GET)
-	public ModelAndView moveToDown(@RequestParam int stretchOrderId, @RequestParam int processionId) {
+	public ModelAndView moveToDown(@RequestParam int stretchOrderId,
+			@RequestParam int processionId) {
 		ModelAndView result;
 		Procession procession;
 		StretchOrder stretchOrder;
 		Collection<StretchOrder> stretchOrders;
-		
+
 		procession = processionService.findOne(processionId);
 		stretchOrder = stretchOrderService.findOne(stretchOrderId);
 		stretchOrders = stretchOrderService.findByProcession(procession);
 		stretchOrderService.moveToDown(stretchOrder, stretchOrders);
-		
-		result = new ModelAndView("redirect:/stretchOrder/list.do?processionId=" + processionId);
-		
+
+		result = new ModelAndView(
+				"redirect:/stretchOrder/list.do?processionId=" + processionId);
+
 		return result;
 	}
-	
+
 	@RequestMapping(value = "/delete", method = RequestMethod.GET)
-	public ModelAndView delete(@RequestParam int stretchOrderId, @RequestParam int processionId) {
+	public ModelAndView delete(@RequestParam int stretchOrderId,
+			@RequestParam int processionId) {
 		ModelAndView result;
 		Procession procession;
 		StretchOrder stretchOrder;
 		Collection<StretchOrder> stretchOrders;
-		
+
 		procession = processionService.findOne(processionId);
 		stretchOrder = stretchOrderService.findOne(stretchOrderId);
 		stretchOrders = stretchOrderService.findByProcession(procession);
 		stretchOrderService.deleteAndReorder(stretchOrder, stretchOrders);
-		
-		result = new ModelAndView("redirect:/stretchOrder/list.do?processionId=" + processionId);
-		
+
+		result = new ModelAndView(
+				"redirect:/stretchOrder/list.do?processionId=" + processionId);
+
 		return result;
 	}
+
+	// Ancillary methods ------------------------------------------------------
 
 }
