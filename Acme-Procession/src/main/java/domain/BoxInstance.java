f6cf1bd8c21739f2457c400de8acde483e80b2cf
mervalmor <mervalmor@0d384962-61ac-47a6-b884-4177c4ba1e9f>
2015-05-18 10:18:09 +0000

    Corregido un error

--- Acme-Procession/src/main/java/domain/BoxInstance.java
+++ Acme-Procession/src/main/java/domain/BoxInstance.java
@@ -12,7 +12,6 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
