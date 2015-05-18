9c69a434a7eb5e771106f0569cdca8f4f2f67d20
mervalmor <mervalmor@0d384962-61ac-47a6-b884-4177c4ba1e9f>
2015-05-18 16:45:50 +0000

    Corregidos algunos errores

--- Acme-Procession/src/main/java/services/BoxReserveService.java
+++ Acme-Procession/src/main/java/services/BoxReserveService.java
@@ -1,6 +1,7 @@
 package services;
 
 import java.util.Collection;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import repositories.BoxReserveRepository;
+import domain.Box;
+import domain.BoxReserve;
 import domain.BoxReserve;
 import domain.Viewer;
 
@@ -34,6 +37,29 @@ public class BoxReserveService {
 	}
 
 	// Simple CRUD methods ----------------------------------------------------
+	
+	public BoxReserve create() {
+		BoxReserve result;
+
+		Assert.isTrue(actorService.isViewer());
+
+		result = new BoxReserve();
+		
+		return result;
+	}
+
+	public BoxReserve save(BoxReserve boxReserve) {
+		BoxReserve result;
+		
+		Assert.notNull(boxReserve);
+		Assert.isTrue(actorService.isViewer());
+
+		result = boxReserveRepository.save(boxReserve);
+
+		return result;
+	}
+	
+	
 	public BoxReserve findOne(int boxReserveId) {
 		BoxReserve result;
 
@@ -49,6 +75,7 @@ public class BoxReserveService {
 
 		return result;
 	}
+	
 	// Other business methods -------------------------------------------------
 	
 	public Collection<BoxReserve> findByPrincipal(){
