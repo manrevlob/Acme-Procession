c09c54fba971ddc87ae2bb3b4ebf954d6dd5e5da
mervalmor <mervalmor@0d384962-61ac-47a6-b884-4177c4ba1e9f>
2015-05-26 16:31:43 +0000

    Corregida una query de BoxInstanceRepository

--- Acme-Procession/src/main/java/repositories/BoxInstanceRepository.java
+++ Acme-Procession/src/main/java/repositories/BoxInstanceRepository.java
@@ -1,7 +1,6 @@
 package repositories;
 
 import java.util.Collection;
-import java.util.Date;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,6 +15,6 @@ public interface BoxInstanceRepository extends
 	@Query("select b from BoxInstance b where b.box.id = ?1")
 	Collection<BoxInstance> findByBox(int boxId);
 	
-	@Query("select b.date from BoxInstance b where b.box.id = ?1")
-	Collection<Date> findDatesAvailablesByBox(int boxId);
+	@Query("select b from BoxInstance b where b.box.id = ?1 and b.availableChairs >=1")
+	Collection<BoxInstance> findAvailablesByBox(int boxId);
 }
